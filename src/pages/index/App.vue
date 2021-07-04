1690cf3d8ecd9146caf28dc08ba03dfdef83b5e5
wanglianke <wanglianke@corp.netease.com>
2021-07-04 12:11:46 +0800

    feat: add tailwindcss postcss

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <img alt="Vue logo" src="@/assets/logo.png" />
+  <img
+    alt="Vue logo"
+    src="@/assets/logo.png"
+    class="mx-auto"
+  >
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
@@ -20,8 +24,7 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  @apply mt-16 text-center;
 }
 </style>
